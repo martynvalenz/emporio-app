f8bb728f262d92e1aeeac1bdc02d1cfc414f05cf
martynvalenz <martynvalenz@gmail.com>
2020-01-28 08:39:06 -0700

    first commit

---
+++ src/boot/axios.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import axios from 'axios'
+import {LocalStorage} from 'quasar'
+
+Vue.prototype.$axios = axios
+
+const token = LocalStorage.getItem('token')
+if(token){
+   Vue.prototype.$axios.defaults.headers.common['Authorization'] = token
+}
+
+export {axios}
